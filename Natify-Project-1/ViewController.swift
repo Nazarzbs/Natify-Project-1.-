032bd9b997bc4e56d55a6348b16d761b3dc59876
Nazar <famina1998@gmail.com>
2026-02-17 22:08:01 +0200

    Initial Commit

---
+++ Natify-Project-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Natify-Project-1
+//
+//  Created by Nazar on 17.02.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
